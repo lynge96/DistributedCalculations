8a44198188b4f8c13519e90d48b097667ac387d5
Anders Lynge Ravnsbæk <90785699+lynge96@users.noreply.github.com>
2026-02-17 20:04:48 +0100

    rabbit mq docker compose

--- src/Calculator.Infrastructure/RabbitMq/RabbitMqEventBus.cs
+++ src/Calculator.Infrastructure/RabbitMq/RabbitMqEventBus.cs
@@ -1,11 +1,17 @@
 ﻿using Calculator.Application.Interfaces;
+using RabbitMQ.Client;
 
 namespace Calculator.Infrastructure.RabbitMq;
 
 public class RabbitMqEventBus : IEventBus
 {
-    public Task PublishAsync<TEvent>(TEvent @event)
+    public async Task PublishAsync<TEvent>(TEvent @event)
     {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+        
         throw new NotImplementedException();
     }
 }
